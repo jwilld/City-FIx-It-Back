0fedc0a84e8ca475d9de27202590a0d93fd163d3
Jamie Miller <miller0328@gmail.com>
2019-08-19 13:52:27 -0400

    agencies controller

--- controllers/agencies.js
+++ controllers/agencies.js
@@ -1,2 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const Agency = require('../models/Agency')
+
+module.exports = {
+    show: (req, res) => Agency.find({})
+        .then(agency => res.json(agency))
+}
+
+// find out if i need to add any others? 
