7fd53f3be29cd291a1645aef8527fa4a2cab00ac
Jamie Miller <miller0328@gmail.com>
2019-08-19 14:02:37 -0400

    tickets route

---
+++ routes/tickets.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const ticketController = require('../controllers/tickets')
+
+router.post('/create', ticketController.create)
+router.get('/', ticketController.show)
+router.put('/:name', ticketController.update)
+router.delete('/delete/:name', ticketController.delete)
+
+module.exports = router
