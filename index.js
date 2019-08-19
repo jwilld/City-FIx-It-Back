4fdb2a425974a1c97782a8df391ae0f4e5254dfa
Jamie Miller <miller0328@gmail.com>
2019-08-19 14:15:00 -0400

    edited index

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const parser = require('body-parser')
 const cors = require('cors')
-
-//eventually add in controllers 'const controller' here once filled out
-
 const app = express()
 
 app.use(cors())
 app.use(parser.json())
 
-//eventually add in controllers 'app.use' here once filled out
+app.use('/agencies', require('./routes/agencies'))
+app.use('/tickets', require('./routes/tickets'))
 
 app.listen(3001, () => console.log("listening on port 3001"))
