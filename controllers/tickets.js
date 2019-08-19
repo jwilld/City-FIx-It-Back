1863a5585a2dae3c499aad39569af7b6577542f3
Jamie Miller <miller0328@gmail.com>
2019-08-19 13:47:03 -0400

    edited tickets controller

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,1 +1,12 @@
 const Ticket = require('../models/Ticket')
+
+module.exports = {
+    create: (req, res) => Ticket.create(req.body)
+        .then(ticket => res.json(ticket)),
+    show: (req, res) => Ticket.find({})
+        .then(ticket => res.json(ticket)),
+    update: (req, res) => Ticket.findOneAndUpdate({ name: req.params.name }, req.body)
+        .then(ticket => res.json(ticket)),
+    delete: (req, res) => Ticket.findOneAndDelete({ name: req.params.name })
+        .then(ticket => res.json(ticket))
+}
