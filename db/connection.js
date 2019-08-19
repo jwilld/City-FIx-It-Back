af3de6360bc56248b1ab561996f5c3021666cbbe
Jamie Miller <miller0328@gmail.com>
2019-08-19 16:24:55 -0400

    working on mongodb stuff

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,20 @@
+// const mongoose = require('mongoose')
+
+// mongoose.Promise = Promise 
+
+// mongoose.connect('mongodb://localhost/city-fix-it')
+//     .then(connection => console.log("connection established"))
+//     .catch(err => console.log("connection failed",err))
+
+// module.exports = mongoose 
+
 const mongoose = require('mongoose')
+let mongoURI = ""
 
-mongoose.Promise = Promise 
-
-mongoose.connect('mongodb://localhost/city-fix-it')
-    .then(connection => console.log("connection established"))
-    .catch(err => console.log("connection failed",err))
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost/city-fix-it";
+}
 
 module.exports = mongoose
--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.use(parser.json())
 app.use('/agencies', require('./routes/agencies'))
 app.use('/tickets', require('./routes/tickets'))
 
-app.listen(3001, () => console.log("listening on port 3001"))
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log(`PORT: ${app.get("port")}`);
+});
