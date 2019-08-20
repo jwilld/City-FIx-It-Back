7a0176cfb74c0ffd39c33343bcc0b3f8e4560468
“David <davejh76@gmail.com>
2019-08-20 16:09:06 -0400

    geocode api tests

--- db/maps/maptest.js
+++ db/maps/maptest.js
@@ -1,5 +1,51 @@
 //make sure this is connected correct
+const mongoose = require('mongoose');
 const connection = require("../../db/connection");
 const Ticket = require('../../models/Ticket')
 
-console.log(Ticket)
+//original non-looped version
+// const TicketData = Ticket.find(function (err, tickets) {
+//     if (err) return console.error(err);
+//     console.log(tickets.length);
+//     thisStreet = tickets[0].Address.Street
+//     thisCity = tickets[0].Address.City
+//     thisZip = tickets[0].Address.Zipcode
+//     thisState = tickets[0].Address.State
+//     thisAddress = thisStreet + ", " + thisCity + " " + thisState + " " + thisZip
+//     console.log(thisAddress)
+//   })
+
+//this does not store the variable globally
+// const TicketData = Ticket.find(function (err, tickets) {
+//     if (err) return console.error(err);
+//     const ticketAddressesArray = []
+//     for (let i = 0; i < tickets.length; i++) {
+//         thisStreet = tickets[i].Address.Street
+//         thisCity = tickets[i].Address.City
+//         thisZip = tickets[i].Address.Zipcode
+//         thisState = tickets[i].Address.State
+//         thisAddress = thisStreet + ", " + thisCity + ", " + thisState + " " + thisZip
+
+//         // console.log(thisAddress)
+//         ticketAddressesArray.push(thisAddress)
+//         console.log(ticketAddressesArray)
+//     }   
+//   })
+
+
+const TicketData = Ticket.find(function (err, tickets) {
+        if (err) return console.error(err);
+        const ticketAddressesArray = []
+        for (let i = 0; i < tickets.length; i++) {
+            thisStreet = tickets[i].Address.Street
+            thisCity = tickets[i].Address.City
+            thisZip = tickets[i].Address.Zipcode
+            thisState = tickets[i].Address.State
+            thisAddress = thisStreet + ", " + thisCity + ", " + thisState + " " + thisZip
+    
+            // console.log(thisAddress)
+            ticketAddressesArray.push(thisAddress)
+            console.log(ticketAddressesArray)
+        }   
+      })
+    
