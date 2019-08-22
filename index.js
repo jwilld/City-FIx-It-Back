d9ab45adb606d10471eeba256ac6f6cd470ea2c3
“David <davejh76@gmail.com>
2019-08-22 09:32:34 -0400

    changes for deploy

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ app.use('/tickets', require('./routes/tickets'))
 
 app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get("port"), () => {
-    console.log(`PORT: ${app.get("port")}`);
-});
+// app.listen(app.get("port"), () => {
+//     console.log(`PORT: ${app.get("port")}`);
+// });
 
 
-// app.listen(3001, () => console.log("listening on port 3001"))
+app.listen(3001, () => console.log("listening on port 3001"))
 
 
 
 //DO NOT ERASE THIS UNTIL PROJECT COMPLETION
 // heroku config:set DB_URL="mongodb+srv://JerryDavidJamie:jdj123@cluster0-8mjuz.mongodb.net/test?retryWrites=true"
 
+// heroku config:set DB_URL="mongodb+srv://JerryDavidJamie:jdj123@cluster0-8mjuz.mongodb.net/test?retryWrites=true"
 
 
 
