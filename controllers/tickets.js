e46fd978fe9606d3d3b1edd09d883f5b3977cbad
Jerry Davidson <willdjerry@gmail.com>
2019-08-22 00:13:03 -0400

    Revert " fixed connection"
    This reverts commit 030d612ceb02498ae7387c28ff7963447abbb2f3.

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,17 +1,14 @@
-const Ticket = require("../models/Ticket");
+const Ticket = require('../models/Ticket')
 
 module.exports = {
-  create: (req, res) =>
-    Ticket.create(req.body).then(ticket => res.json(ticket)),
-  show: (req, res) => Ticket.find({}).then(ticket => res.json(ticket)),
-  showId: (req, res) =>
-    Ticket.findOne({ _id: req.params.id }).then(ticket => res.json(ticket)),
-  update: (req, res) =>
-    Ticket.findOneAndUpdate({ name: req.params.name }, req.body).then(ticket =>
-      res.json(ticket)
-    ),
-  delete: (req, res) =>
-    Ticket.findOneAndDelete({ name: req.params.name }).then(ticket =>
-      res.json(ticket)
-    )
-};
+    create: (req, res) => Ticket.create(req.body)
+        .then(ticket => res.json(ticket)),
+    show: (req, res) => Ticket.find({})
+        .then(ticket => res.json(ticket)),
+    showId:(req,res) => Ticket.findOne({_id:req.params.id})
+    .then(ticket => res.json(ticket)),
+    update: (req, res) => Ticket.findOneAndUpdate({ name: req.params.name }, req.body)
+        .then(ticket => res.json(ticket)),
+    delete: (req, res) => Ticket.findOneAndDelete({ name: req.params.name })
+        .then(ticket => res.json(ticket))
+}
