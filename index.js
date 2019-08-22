97e590b6c0a250ed852b67f9cffb4310b9bdf0a2
“David <davejh76@gmail.com>
2019-08-22 09:21:28 -0400

    revert index port settings back

--- index.js
+++ index.js
@@ -10,22 +10,19 @@ app.use(parser.json())
 app.use('/agencies', require('./routes/agencies'))
 app.use('/tickets', require('./routes/tickets'))
 
-// app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
-// app.listen(app.get("port"), () => {
-//     console.log(`PORT: ${app.get("port")}`);
-// });
-
-
-app.listen(3001, () => console.log("listening on port 3001"))
+app.listen(app.get("port"), () => {
+    console.log(`PORT: ${app.get("port")}`);
+});
 
 
-// mongodb+srv://JerryDavidJamie:JerryDavidJamie???@cluster0-rajxf.mongodb.net/test?retryWrites=true&w=majority
+// app.listen(3001, () => console.log("listening on port 3001"))
 
 
-// mongodb+srv://JerryDavidJamie:JerryDavidJamie???@cluster0-rajxf.mongodb.net/test?retryWrites=true&w=majority
 
-// heroku config:set DB_URL="mongodb+srv://JerryDavidJamie:JerryDavidJamie???@cluster0-8mjuz.mongodb.net/test?retryWrites=true"
+//DO NOT ERASE THIS UNTIL PROJECT COMPLETION
+// heroku config:set DB_URL="mongodb+srv://JerryDavidJamie:jdj123@cluster0-8mjuz.mongodb.net/test?retryWrites=true"
 
 
 
