274e4a87675183d5e8e86f9592e7119ecd65be46
“David <davejh76@gmail.com>
2019-08-22 10:58:48 -0400

    model change to address

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,13 +1,7 @@
 const mongoose = require('../db/connection')
 
 const Ticket = new mongoose.Schema({
-    address: {
-        street: String,
-        apartment: String,
-        city: String,
-        zipcode: String,
-        state: String
-    },
+    address: String,
     type: String,
     priority: String,
     description: String,
